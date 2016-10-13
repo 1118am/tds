501bb92814e959301de563268ce59fbeed838a92
1118am <callmeallstate@gmail.com>
2016-10-12 22:35:57 -0700

    Added Following Enemy (No Functionality Yet)

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(NavMeshAgent))]
+public class Enemy : MonoBehaviour {
+
+	NavMeshAgent pathfiner;
+	Transform target;
+
+	void Start () {
+		pathfiner = GetComponent<NavMeshAgent> ();
+		target = GameObject.FindGameObjectWithTag ("Player").transform;
+		StartCoroutine (UpdatePath());
+	}
+	
+	void Update () {
+		
+	}
+
+	IEnumerator UpdatePath () {
+		float refreshRate = 0.25f;
+		while (target != null) {
+			Vector3 targetPostion = new Vector3 (target.position.x, 0, target.position.z);
+			pathfiner.SetDestination (targetPostion);
+			yield return new WaitForSeconds (refreshRate);
+		}
+	}
+
+}
